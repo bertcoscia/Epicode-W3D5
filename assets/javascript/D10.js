1ac1d185bf82f7b79805700e0ac174971977f6d1
A. Alberto Coscia <angeloalberto.coscia@gmail.com>
2024-05-17 16:10:19 +0200

    answered q 20

--- assets/javascript/D10.js
+++ assets/javascript/D10.js
@@ -579,6 +579,8 @@ removeIndex(0);
   Scrivi una funzione per selezionare l'elemento dotato di id "container" all'interno della pagina.
 */
 
+const container = document.getElementById("container");
+
 /* ESERCIZIO 21
   Scrivi una funzione per selezionare ogni tag <td> all'interno della pagina.
 */
